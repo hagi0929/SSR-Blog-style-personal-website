218f82b641a400583ec1752c3ef6ae2bafba75af
jaehak kim <ministove3yo@gmail.com>
2024-08-22 19:07:24 -0400

    route setup

--- src/app/article/[id]/page.tsx
+++ src/app/article/[id]/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
-export default function Article() {
+export default function Article({ params }: { params: { id: string } }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      sup
+      {params.id}
     </main>
   );
 }
