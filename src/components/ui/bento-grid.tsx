cb144b8dc532471987d7bbfb6e070b5861ec1258
seansongss <seansongss@gmail.com>
2024-08-28 14:07:11 +0900

    add border color to bento-grid

--- src/components/ui/bento-grid.tsx
+++ src/components/ui/bento-grid.tsx
@@ -39,7 +39,7 @@ export const BentoGridItem = ({
 }) => {
   return (
     <Link href={href || ""} passHref className="block h-full">
-      <Card className="h-full row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4">
+      <Card className="h-full row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-muted justify-between flex flex-col space-y-4">
         <CardHeader>
           <CardTitle>{title}</CardTitle>
           <CardDescription>{description}</CardDescription>
