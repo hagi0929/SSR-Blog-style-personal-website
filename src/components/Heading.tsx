32d4f69192300f9e9de758710f3f9c334cf0e624
seansongss <seansongss@gmail.com>
2024-08-28 14:06:15 +0900

    heading css edit

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Heading = ({ heading, subheading} : { heading: string, subheading: string}) => {
   return (
-    <div className="w-full flex flex-col justify-start py-10 gap-4">
+    <div className="w-full flex flex-col justify-start mt-10 mb-6 gap-4">
         <span className="text-3xl font-bold">
             {heading}
         </span>
-        <span className="text-lg">
+        <span className="text-lg font-medium">
             {subheading}
         </span>
     </div>
