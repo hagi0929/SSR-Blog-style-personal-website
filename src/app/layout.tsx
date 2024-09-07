548538f4afa3aec42a0f5cb354c1de219c640852
jaehak kim <ministove3yo@gmail.com>
2024-09-07 12:08:22 -0400

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter as FontSans } from "next/font/google"
 import "./globals.css";
 
 import { cn } from "@/lib/utils"
-import Tab from "@/components/Header/Tab";
+import Tab from "@/components/Tab";
 import Footer from "@/components/Footer";
 
 const fontSans = FontSans({
