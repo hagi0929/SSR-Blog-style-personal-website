e86f71603f7cd2a393fdbc81eb40f0ac004f2149
seansongss <seansongss@gmail.com>
2024-09-09 13:00:53 +0900

    footer align

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer: React.FC = () => {
     return (
-        <div className="py-8 justify-center">
+        <div className="flex flex-col py-8 justify-center items-center">
             <p>
                 © 2024 <b>Designed & Made</b> with 🤍 by <b>Jaehak</b> and <b>Sean</b>
             </p>
