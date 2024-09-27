a6ee6ee336bd79eadf45fdcf2ccb6552386bdc29
seansongss <seansongss@gmail.com>
2024-09-28 07:59:11 +0900

    change to test page

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,32 +1,19 @@
-import { getDatabase, getData, getPageData } from "@/api/notion"
-import { NotionAPI } from 'notion-client'
-import { NotionRenderer } from 'react-notion-x'
+import { notion, getData } from "@/api/notion"
 // notion 테마 스타일링 (필수)
 import 'react-notion-x/src/styles.css'
 
-// 코드 하이라이트 스타일용 (선택)
-import 'prismjs/themes/prism-tomorrow.css'
+import NotionPage from "@/components/NotionPage"
 
 export default async function ArticlePage() {
     const db = await getData();
 
-    console.log(db.results[1]);
-
-    const notion = new NotionAPI({
-        activeUser: process.env.NOTION_ACTIVE_USER,
-        authToken: process.env.NOTION_TOKEN_V2
-    })
-    const recordMap = await notion.getPage(db.results[1].id);
-
-    console.log("recordMap: ", recordMap);
-    console.log("block: ", recordMap.block['b910db21-d291-4a05-a885-85153b2436c3'].value);
+    const recordMap = await notion.getPage(db.results[0].id);
 
     return (
         <>
-            <NotionRenderer recordMap={recordMap} />
-            <div>
-                success
-            </div>
+            <NotionPage 
+                recordMap={recordMap}
+            />
         </>
     );
 }
