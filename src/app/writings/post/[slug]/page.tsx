0976bae5c0ebc3d6f07029bb5097a9bcf717227e
seansongss <seansongss@gmail.com>
2024-09-28 08:01:52 +0900

    change article page

--- src/app/writings/post/[slug]/page.tsx
+++ src/app/writings/post/[slug]/page.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { NotionRenderer } from 'react-notion-x';
 import { getContent } from '@/api/writings';
-import {NotionPage} from "@/components/NotionPage";
+import NotionPage from '@/components/NotionPage';
 
 export default async function Page({ params }: { params: { slug: string } }) {
     const content = await getContent(params.slug);
 
     return (
-        <div>
-            content
-        </div>
+        <>
+            <NotionRenderer recordMap={content} />
+        </>
     );
 }
