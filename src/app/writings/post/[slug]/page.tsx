49f6e65b402cdf1a95a99412db25cc3c27bea419
seansongss <seansongss@gmail.com>
2024-09-28 08:50:13 +0900

    done

--- src/app/writings/post/[slug]/page.tsx
+++ src/app/writings/post/[slug]/page.tsx
@@ -5,7 +5,9 @@ import NotionPage from '@/components/NotionPage';
 export default async function Page({ params }: { params: { slug: string } }) {
     const data = await getContent(params.slug);
     const content = data[0].content;
+
     console.log(content);
+    
     return (
         <>
             <NotionPage recordMap={content} />
