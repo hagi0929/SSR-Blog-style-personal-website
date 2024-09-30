68f55239dac6009d83f9d984f5c9bf63ee33271f
Sean Song <seansongss@gmail.com>
2024-09-30 16:55:47 +0900

    update supabase getArticle with changing type and return one or no element

--- src/utils/api/supabase.ts
+++ src/utils/api/supabase.ts
@@ -29,9 +29,10 @@ export async function getItemsByItemName(itemName: string): Promise<{ data: any[
   }
 }
 
-export async function getArticleContentFromSlug(itemName: string,slug: string): Promise<{ data: any[] | null; error: any }> {
+export async function getArticleContentFromSlug(itemName: string, slug: string): Promise<{ data: any | null; error: any }> {
   try {
-    const { data, error } = await supabaseClient.from('ItemList').select('*').eq('itemName', itemName).eq('slug', slug);
+    const { data, error } = await supabaseClient.from('ItemList').select('*').eq('itemName', itemName).eq('slug', slug).limit(1).maybeSingle();
+    console.log("data: " + JSON.stringify(data));
     return { data, error };
   } catch (err) {
     console.error("Error fetching items:", err);
