153fce33e7a362d5c3cb4881953ec5c00c533a47
Sean Song <seansongss@gmail.com>
2024-09-30 14:31:54 +0900

    add data

--- src/app/writings/post/[slug]/page.tsx
+++ src/app/writings/post/[slug]/page.tsx
@@ -4,9 +4,9 @@ import NotionPage from '@/components/NotionPage';
 
 export default async function Page({ params }: { params: { slug: string } }) {
     const data = await getContent(params.slug);
-    const content = data[0].content;
+    const content = JSON.parse(data[0].content);
 
-    console.log(content);
+    console.log("data " + data);
     
     return (
         <>
