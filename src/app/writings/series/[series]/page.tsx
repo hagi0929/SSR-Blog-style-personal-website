72c1b4aff2c954e85cfb30dcb11f6d686afa9ae4
seansongss <seansongss@gmail.com>
2024-09-30 22:30:04 +0900

    edit series to be static

--- src/app/writings/series/[series]/page.tsx
+++ src/app/writings/series/[series]/page.tsx
@@ -1,44 +1,61 @@
-import Image from "next/image";
-import { BentoGrid, BentoGridItem } from "@/components/ui/bento-grid";
-import { motion } from "framer-motion";
-import { cn } from "@/lib/utils";
-import Link from "next/link";
-import ProjectList from "@/components/Project/ProjectList";
-import ProjectFilterBar from "@/components/Project/ProjectFilterBar";
-import Heading from "@/components/Heading";
 import { Card, CardContent } from "@/components/ui/card"
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel"
-import { exampleArticles } from "@/data/newMockData";
-import { getWritings } from "@/api/writings";
+import Heading from "@/components/Heading";
+import BackButton from "@/components/BackButton";
 
-export default async function Writings({ params }: { params: { category?: string[] } }) {
-  const projects = await getWritings(params.category);
-  // const tags = await getArticleTagList();
+import { getWritings, getWritingSeriesList } from "@/api/writings";
+
+import { ProjectTechStackModel } from "@/models/models";
+
+export const dynamicParams = false;
+
+export async function generateStaticParams() {
+  const seriesList: ProjectTechStackModel[] = await getWritingSeriesList();
+  console.log(seriesList);
+  return seriesList.map(series => ({
+    series: series.slug,
+  }));
+}
+
+export default async function Writings({ params }: { params: { series: string } }) {
+  const projects = await getWritings(params.series);
+
+  console.log(projects);
 
   return (
-    <div className="w-full flex flex-col min-h-screen gap-16">
-      <Link href={"/writings/"}>
-        Back
-      </Link>
+    <div className="w-full h-full flex flex-col gap-16">
+      <div className="flex -mb-10">
+        <BackButton />
+      </div>
 
       <Heading
-        heading="All Posts"
+        heading={`Series: ${params.series}`}
         subheading="These are list of my writings. Most of them would be technical stuff"
       />
-      <div className="flex flex-col gap-4">
-        {projects.map(({ id, title, slug, createdAt, previewText }, index) => {
-          const url = "/post/" + slug;
-          const style = index % 3 == 0 ? "col-span-2" : "col-span-1";
-          return (
-            <div>{slug}, {title}</div>
-          )
-        })}
+
+      <div className="flex flex-grow flex-col gap-4">
+        {projects && projects.length > 0 ? (
+          projects.map(({ id, title, slug, createdAt, previewText }, index) => {
+            const url = "/post/" + slug;
+            const style = index % 3 == 0 ? "col-span-2" : "col-span-1";
+            return (
+              <div key={id} className={style}>
+                <Card>
+                  <CardContent>
+                    <a href={url}>
+                      <h3 className="font-bold">{title}</h3>
+                      <p>{createdAt.toString()}</p>
+                      <p>{previewText}</p>
+                    </a>
+                  </CardContent>
+                </Card>
+              </div>
+            );
+          })
+        ) : (
+          <div className="flex flex-grow items-center justify-center">
+            <p className="text-center text-lg">No articles found in the series &quot;{params.series}&quot;</p>
+          </div>
+        )}
       </div>
     </div>
   );
