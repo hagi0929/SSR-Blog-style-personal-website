f1b467c993d30f6cb4a180fa14d3089ae71224d3
seansongss <seansongss@gmail.com>
2024-09-30 22:29:55 +0900

    edit post page to be static

--- src/app/writings/post/[slug]/page.tsx
+++ src/app/writings/post/[slug]/page.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import { getContent } from '@/api/writings';
+import { ArticleModel } from '@/models/models';
+import { getContent, getWritings } from '@/api/writings';
 import NotionPage from '@/components/NotionPage';
+import BackButton from '@/components/BackButton';
+
+export const dynamicParams = false;
+
+export async function generateStaticParams() {
+    const articles: ArticleModel[] = await getWritings();
+
+    // Return an array of params with slugs
+    return articles.map(article => ({
+        slug: article.slug,
+    }));
+}
 
 export default async function Page({ params }: { params: { slug: string } }) {
     const content = await getContent(params.slug);
 
-    console.log("data " + content);
-    
     return (
         <>
+            <div className="flex max-w-[720px] mx-auto">
+                <BackButton />
+            </div>
             <NotionPage recordMap={content} />
         </>
     );
