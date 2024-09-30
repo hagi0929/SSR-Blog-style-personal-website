23ffe6500cf622919f7e3167558dd5aebf2450fa
Sean Song <seansongss@gmail.com>
2024-09-30 16:56:44 +0900

    delete comment

--- src/utils/api/supabase.ts
+++ src/utils/api/supabase.ts
@@ -32,7 +32,6 @@ export async function getItemsByItemName(itemName: string): Promise<{ data: any[
 export async function getArticleContentFromSlug(itemName: string, slug: string): Promise<{ data: any | null; error: any }> {
   try {
     const { data, error } = await supabaseClient.from('ItemList').select('*').eq('itemName', itemName).eq('slug', slug).limit(1).maybeSingle();
-    console.log("data: " + JSON.stringify(data));
     return { data, error };
   } catch (err) {
     console.error("Error fetching items:", err);
