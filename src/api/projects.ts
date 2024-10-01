fb7ea5bc0bf50c0d967879a10f4f558271170386
seansongss <seansongss@gmail.com>
2024-10-01 16:21:20 +0900

    add getprojectcontent

--- src/api/projects.ts
+++ src/api/projects.ts
@@ -1,8 +1,9 @@
 import { metadata } from "@/app/layout";
 import { ProjectCategoryModel, ProjectModel, ProjectTechStackModel } from "@/models/models";
-import { getItemsByItemName, getPropertiesByItemName } from "@/utils/api/supabase";
+import { getItemsByItemName, getPropertiesByItemName, getArticleContentFromSlug } from "@/utils/api/supabase";
 import { log } from "console";
 import { NextURL } from "next/dist/server/web/next-url";
+import { ExtendedRecordMap } from "notion-types";
 
 export async function getProjects(
   categoryLabel: string | null = null,
@@ -27,10 +28,24 @@ export async function getProjects(
       categories: item.properties?.filter((property) => property.name === "Category") || [],
     } as ProjectModel;
   })
-  console.log(parsedItems);
 
   return parsedItems as ProjectModel[];
 }
+
+export const getProjectContent = async (slug: string) : Promise<ExtendedRecordMap> => {
+  let { data, error } = await getArticleContentFromSlug("Project", slug);
+
+  if (error) {
+      // TODO: Handle error
+      throw new Error("Error while fetching : " + error + " data: " + data);
+  }
+  
+  if (!data) {
+      throw new Error("No data found for slug: " + slug);
+  }
+
+  return JSON.parse(data.content) as ExtendedRecordMap;
+};
 
 export async function getProjectCategoryList(): Promise<ProjectCategoryModel[]> {
   const { data, error } = await getPropertiesByItemName("Project", "Category");
@@ -41,6 +56,7 @@ export async function getProjectCategoryList(): Promise<ProjectCategoryModel[]> 
 
   return data.map((singleData: any) => {
     return {
+
       id: singleData.id,
       label: singleData.label,
       slug: singleData.slug,
