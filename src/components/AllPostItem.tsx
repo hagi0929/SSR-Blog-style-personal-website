231045eb6185c705b605167f4619c7048e4f0cb5
seansongss <seansongss@gmail.com>
2024-10-01 11:59:55 +0900

    add AllPostItem

--- src/components/AllPostItem.tsx
+++ src/components/AllPostItem.tsx
@@ -1,23 +1,61 @@
 import React from 'react'
-import { Card, CardDescription, CardHeader, CardTitle } from './ui/card'
+import Link from 'next/link'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { ArticleTagModel, ProjectTechStackModel } from '@/models/models';
+import { ArrowRight } from 'lucide-react';
+import { Badge } from '@/components/ui/badge';
 
 interface AllPostItemProps {
-    title: string;
-    description: string;
-    date: Date;
-    url: string;
-    tags: ArticleTagModel[] | ProjectTechStackModel[];
+  title: string;
+  description: string;
+  date: Date;
+  url: string;
+  tags: ArticleTagModel[] | ProjectTechStackModel[];
 }
 
 const AllPostItem: React.FC<AllPostItemProps> = ({ title, description, date, url, tags }) => {
   return (
-    <Card>
+    <Link href={url}>
+      <Card className="">
         <CardHeader>
-            <CardDescription></CardDescription>
-            <CardTitle></CardTitle>
+          <CardDescription>
+            Test
+          </CardDescription>
+          <CardTitle>{title}</CardTitle>
+          <CardDescription>test one</CardDescription>
         </CardHeader>
-    </Card>
+        <CardContent>
+        </CardContent>
+        <CardFooter className="justify-between">
+          <CardDescription className="flex gap-1">
+            {tags.map((tag) => (
+              <Badge
+                key={tag.label}
+                variant={"outline"}
+              >
+                {tag.label}
+              </Badge>
+            ))}
+            <Badge
+              variant={"outline"}
+              className="text-xs py-0 leading-normal"
+            >
+              Javascript
+            </Badge>
+            <Badge
+              variant={"outline"}
+              className="text-xs py-0 leading-normal"
+            >
+              test2
+            </Badge>
+          </CardDescription>
+          <CardDescription className="flex flex-row gap-1 justify-center items-center">
+            Read More
+            <ArrowRight className="h-4 w-4" />
+          </CardDescription>
+        </CardFooter>
+      </Card>
+    </Link>
   )
 }
 
