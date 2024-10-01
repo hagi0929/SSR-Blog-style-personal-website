b3b6a7ba36dd323e31ce33a7563b04fa4eabb65d
seansongss <seansongss@gmail.com>
2024-10-01 16:22:41 +0900

    add optional subheading type

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
-const Heading = ({ heading, subheading} : { heading: string, subheading: string}) => {
+const Heading = ({ heading, subheading }: { heading: string, subheading?: string }) => {
   return (
     <div className="w-full flex flex-col justify-start gap-4">
-        <span className="text-3xl font-bold">
-            {heading}
-        </span>
+      <span className="text-3xl font-bold">
+        {heading}
+      </span>
+      {subheading && (
         <span className="text-lg font-medium">
-            {subheading}
+          {subheading}
         </span>
+      )}
     </div>
   )
 }
